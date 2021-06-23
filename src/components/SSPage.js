42e15df986b26e12a688d7ea9863361bc8a8b3c0
Kelly Nauert <kellynauert@gmail.com>
2021-06-23 12:35:34 -0400

    font change

--- src/components/SSPage.js
+++ src/components/SSPage.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Grid, Typography } from '@material-ui/core';
 import SSDataCard from './SSDataCard';
 import SectionTitle from './SectionTitle';
+import CRELogo from '../images/creLogo.png';
 const SSPage = (props) => {
   console.log(props);
   return (
@@ -11,11 +12,10 @@ const SSPage = (props) => {
         item
         sm={12}
         direction='row'
-        // spacing={2}
         style={{
           border: '10px solid #00aeef',
           borderRadius: '12px',
-          padding: '8px 18px',
+          padding: '16px 16px 36px 16px',
           marginTop: '16px',
         }}
         justify='center'
@@ -25,46 +25,42 @@ const SSPage = (props) => {
           container
           item
           alignItems='center'
-          style={{ margin: '16px 0' }}
           justify='space-between'
+          style={{ marginBottom: '18px' }}
         >
-          <Grid item sm={7}>
-            <Typography variant='h1' style={{ textAlign: 'left' }}>
-              XR Summer School
-            </Typography>
+          <Grid container item sm={6} justify='flex-start'>
+            <img src={CRELogo} alt='Logo' style={{ height: '30px' }} />
           </Grid>
+
           <Grid
             container
             item
-            sm={5}
+            sm={6}
             style={{
-              marginBottom: '8px',
               flexDirection: 'column',
-              textAlign: 'right',
             }}
           >
-            <Typography variant='subtitle1'>{props.student}</Typography>
-            <Typography variant='h5'>Cathedral 2021</Typography>
+            <Typography variant='infoBold'>{props.student}</Typography>
+            <Typography variant='info'>Cathedral High School 2021</Typography>
+          </Grid>
+          <Grid item sm>
+            <Typography variant='pageHeader'>
+              XR Summer School Report
+            </Typography>
           </Grid>
         </Grid>
-        <Grid
-          container
-          style={{ textAlign: 'left', marginBottom: '8px' }}
-          spacing={2}
-        >
+        <Grid container style={{ marginBottom: '8px' }} spacing={2}>
           <Grid item sm={6}>
-            <Typography variant='subtitle2'>Waypoints</Typography>
+            <Typography variant='sub1'>Waypoints</Typography>
 
-            <Typography variant='h5'>
+            <Typography variant='body'>
               An online quiz platform that uses Common Core mathematics
               standards to measure gaps in a diagnostic phase, followed by
               measuring math understanding in the growth phase.
             </Typography>
 
-            <Typography variant='subtitle1' style={{ marginTop: '8px' }}>
-              Growth Check-ins
-            </Typography>
-            <Typography variant='h5'>
+            <Typography variant='sub2'>Growth Check-ins</Typography>
+            <Typography variant='body'>
               At this point, students have discovered their gaps in math
               understanding and have reviewed the material through tasks on
               Mathspace. Here is where they had a chance to fill in those gaps
@@ -73,10 +69,8 @@ const SSPage = (props) => {
               their growth.
             </Typography>
 
-            <Typography variant='subtitle1' style={{ marginTop: '8px' }}>
-              End of Diagnostic Phase
-            </Typography>
-            <Typography variant='h5'>
+            <Typography variant='sub2'>End of Diagnostic Phase</Typography>
+            <Typography variant='body'>
               The Diagnostic Phase consists of 10 check-ins that gives us a base
               grade level estimate for each student. This is the basis from
               which we measure growth.
@@ -84,34 +78,30 @@ const SSPage = (props) => {
           </Grid>
 
           <Grid item sm={6}>
-            <Typography variant='subtitle2'>Mathspace</Typography>
-            <Typography variant='h5'>
+            <Typography variant='sub1'>Mathspace</Typography>
+            <Typography variant='body'>
               An online mathematics curriculum where students completed tasks in
               order to prepare for the growth phase of Waypoints. Students were
               assigned tasks based on their Waypoints gaps, and those tasks
               would adapt to the student’s performance by asking more or less of
               the questions they needed to see.
             </Typography>
-            <Typography variant='subtitle1' style={{ marginTop: '8px' }}>
-              Tasks
-            </Typography>
-            <Typography variant='h5'>
+            <Typography variant='sub2'>Tasks</Typography>
+            <Typography variant='body'>
               Depending on performance in the Diagnostic Phase of Waypoints,
               students were assigned a number of tasks designed to prepare them
               for the gaps they may have had before.
             </Typography>
-            <Typography variant='subtitle1' style={{ marginTop: '8px' }}>
-              Time Spent Working
-            </Typography>
-            <Typography variant='h5'>
+            <Typography variant='sub2'>Time Spent Working</Typography>
+            <Typography variant='body'>
               Based on the task load, we expected at least 30 minutes of working
               time each day from June 7 to June 23.
             </Typography>
           </Grid>
 
           <Grid item sm={6}>
-            <Typography variant='subtitle2'>Nexus</Typography>
-            <Typography variant='h5'>
+            <Typography variant='sub1'>Nexus</Typography>
+            <Typography variant='body'>
               A collaborative tutoring platform where students had access to
               live help from our team of virtual tutors. Students had the
               opportunity to notify a tutor when they had a question, and they
@@ -123,62 +113,53 @@ const SSPage = (props) => {
         <SectionTitle text='Waypoints' />
 
         <SSDataCard
-          gridSpace={3}
           data={props.waypointsCheckins}
           item='Growth Check-ins'
           key='checkins'
         />
         <SSDataCard
-          gridSpace={3}
           data={props.waypointsStart}
           item='Diagnostic Grade Level'
           key='waypoints-start'
         />
         <SSDataCard
-          gridSpace={3}
           data={props.waypointsEnd}
           item='Final Grade Level'
           key='waypoints-end'
         />
         <SSDataCard
-          gridSpace={3}
           data={props.waypointsGrowth}
           item='Grade Lvl Growth'
           key='waypoints-growth'
         />
         <SectionTitle text='Mathspace' />
-
-        <SSDataCard
-          gridSpace={2}
-          data={props.mathspaceTime}
-          item='Total Time'
-          key='mathspace-total-time'
-        />
-        <SSDataCard
-          gridSpace={2}
-          data={props.percentageAttempted}
-          item='Tasks Attempted'
-          key='tasks-attempted'
-        />
-        <SSDataCard
-          gridSpace={2}
-          data={props.percentageCompleted}
-          item='Tasks Completed'
-          key='tasks-completed'
-        />
-        <SSDataCard
-          gridSpace={2}
-          data={props.percentageDays}
-          item='Days Active'
-          key='days'
-        />
-        <SSDataCard
-          gridSpace={2}
-          data={props.mathspaceAvgTime}
-          item='Avg Time Per Day'
-          key='avg-time-days'
-        />
-
+        <Grid container item sm={12}>
+          <SSDataCard
+            data={props.mathspaceTime}
+            item='Total Time'
+            key='mathspace-total-time'
+          />
+          <SSDataCard
+            data={props.percentageAttempted}
+            item='Tasks Attempted'
+            key='tasks-attempted'
+          />
+          <SSDataCard
+            data={props.percentageCompleted}
+            item='Tasks Completed'
+            key='tasks-completed'
+          />
+          <SSDataCard
+            data={props.percentageDays}
+            item='Days Active'
+            key='days'
+          />
+          <SSDataCard
+            data={props.mathspaceAvgTime}
+            item='Avg Time Per Day'
+            key='avg-time-days'
+          />
+        </Grid>
         <Grid container item sm={6} style={{ paddingRight: '24px' }}>
           <SectionTitle text='Nexus' />
           <SSDataCard
@@ -201,7 +182,7 @@ const SSPage = (props) => {
           justify='center'
           style={{ paddingLeft: '24px' }}
         >
-          <SectionTitle text='General' />
+          <SectionTitle text='    General    ' />
           <SSDataCard
             gridSpace={12}
             data={props.totalTime}
